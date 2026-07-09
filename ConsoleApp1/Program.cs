bad513af4062ac79ced9c7251cada61b49fc20d2
San Linn Phyo <devsanlphyo@gmail.com>
2026-07-10 00:49:38 +0630

    Revert "Delete ConsoleApp1 directory"
    This reverts commit dd9e1df3d52d575aee5a53ab8f521760fe62fd92.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Steps
+//1. Create a connection string builder
+//2. Config builder information: userID, password, initialCatalog, database
+//3. Create a connection string from the builder
+//4. Create a query (use @ for multiline)
+//5. Create a sql command from query and connection
+//6. Create an adapter from cmd
+//7. Create a datatable
+//8. Fill the table
+//9. Loop the table
+
+using TestingNameSpace;
+
+AdoNetService service = new AdoNetService();
+//service.Read();
+//service.Create();
+//service.Update();
+service.Delete();  
